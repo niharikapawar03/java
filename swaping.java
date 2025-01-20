a0d940e3265ba1b6091645bcdea731b02c4fbf37
Niharika Pawar <119047940+niharikapawar03@users.noreply.github.com>
2025-01-20 23:38:05 +0530

    Add files via upload

---
+++ swaping.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class swaping {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int temp=0;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
